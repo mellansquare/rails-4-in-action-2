0ecf4d52002743b4f2986ae0574dcfc2d31c3d01
Melanie Packer <melanie.packer@gmail.com>
2018-10-15 06:16:04 +0000

    Add feature for archiving users, including protection against self-archiving

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,12 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  scope :excluding_archived, lambda { where(archived_at: nil) }
   def to_s
     "#{email} (#{admin? ? "Admin" : "User"})"
+  end
+  
+  def archive
+    self.update(archived_at: Time.now)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181010033531) do
+ActiveRecord::Schema.define(version: 20181015055912) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "name"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20181010033531) do
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
     t.boolean  "admin",                  default: false
+    t.datetime "archived_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
