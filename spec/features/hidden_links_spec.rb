530ec812417e10c8df842ee951bbc25acad6821d
Melanie Packer <melanie.packer@gmail.com>
2018-10-19 01:49:59 +0000

    Added permissions for editing tickets

--- spec/features/hidden_links_spec.rb
+++ spec/features/hidden_links_spec.rb
@@ -4,6 +4,9 @@ RSpec.feature "Users can only see the appropriate links" do
   let(:project) { FactoryBot.create(:project) }
   let(:user) { FactoryBot.create(:user) }
   let(:admin) { FactoryBot.create(:user, :admin) }
+  let(:ticket) do
+    FactoryBot.create(:ticket, project: project, author: user)
+  end
 
   context "non-admin users (project viewers)" do
     before do
@@ -30,6 +33,11 @@ RSpec.feature "Users can only see the appropriate links" do
       visit project_path(project)
       expect(page).not_to have_link "New Ticket"
     end
+    
+    scenario "cannot see the Edit Ticket link" do
+      visit project_ticket_path(project, ticket)
+      expect(page).not_to have_link "Edit Ticket"
+    end
   end
 
   context "admin users" do
@@ -48,6 +56,11 @@ RSpec.feature "Users can only see the appropriate links" do
     scenario "can see the New Ticket link" do
       visit project_path(project)
       expect(page).to have_link "New Ticket"
+    end
+    
+    scenario "can see the Edit Ticket link" do
+      visit project_ticket_path(project, ticket)
+      expect(page).to have_link "Edit Ticket"
     end
   end
 end
