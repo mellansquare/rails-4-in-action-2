2669c1a3be9350002853217ddb5def29d8b6d0f4
Melanie Packer <melanie.packer@gmail.com>
2018-11-27 05:45:35 +0000

    Protect state_id from users who do not have permission to change it

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -32,6 +32,7 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
@@ -62,7 +63,7 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
   config.include Warden::Test::Helpers, type: :feature
   config.after(type: :feature) { Warden.test_reset! }
-  
+  config.include Devise::TestHelpers, type: :controller
   # Headless firefox setup
   Capybara.register_driver :firefox_headless do |app|
     options = ::Selenium::WebDriver::Firefox::Options.new
